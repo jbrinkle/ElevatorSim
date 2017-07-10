9f2fd6843eaacc4c71887e382a084ad791b22d3b
Jason Brinkle <jasongb@microsoft.com>
2017-07-10 09:08:52 -0700

    Elevator dispatcher

---
+++ elevatorsim/elevatorsim/Simulator/Elevator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevatorsim.Simulator
+{
+    public class Elevator
+    {
+        public uint Id { get; private set; }
+
+        public Elevator(uint id, IElevatorStatusReceiver statusReceiver)
+        {
+
+        }
+
+        public void AddStop(int floor)
+        {
+
+        }
+    }
+}
---
+++ elevatorsim/elevatorsim/Simulator/ElevatorDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevatorsim.Simulator
+{
+    public class ElevatorDispatcher : IElevatorRequestReceiver
+    {
+        private IElevatorLandscape _landscape;
+        private List<Elevator> _elevators;
+
+        public ElevatorDispatcher(IElevatorLandscape landscape, List<Elevator> elevators)
+        {
+            _landscape = landscape;
+            _elevators = elevators;
+        }
+
+        public void Request(int floor)
+        {
+            // TODO: error checking
+
+            var eid = _landscape.FindNearestElevatorToFloor(floor);
+            var elevator = _elevators.Single(e => e.Id == eid);
+
+            elevator.AddStop(floor);
+        }
+    }
+}
