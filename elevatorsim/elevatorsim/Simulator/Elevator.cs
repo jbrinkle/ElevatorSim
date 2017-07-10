a5bc65c6b31afa9d8f62d4161f419cb9c630e705
Jason Brinkle <jasongb@microsoft.com>
2017-07-10 09:37:42 -0700

    Updating elevator class... would prefer to finish fleshing out and unit testing before proceeding.

--- elevatorsim/elevatorsim/Simulator/Elevator.cs
+++ elevatorsim/elevatorsim/Simulator/Elevator.cs
@@ -8,16 +8,45 @@ namespace elevatorsim.Simulator
 {
     public class Elevator
     {
+        private int _minFloor;
+        private int _maxFloor;
+        private List<int> _stops;
+
+        public enum IntentStatus
+        {
+            Unoccupied,
+            MovingUp,
+            MovingDown,
+            OutOfService
+        }
+
+        public enum DoorStatus
+        {
+            Open,
+            Closed
+        }
+
         public uint Id { get; private set; }
+
+        public IntentStatus Intent { get; private set; }
+
+        public DoorStatus Door { get; private set; }
+
+        public int CurrentFloor { get; private set; }
 
         public Elevator(IElevatorStatusReceiver statusReceiver, uint id, int minFloor, int maxFloor)
         {
-
+            Id = id;
+            _minFloor = minFloor;
+            _maxFloor = maxFloor;
+            _stops = new List<int>();
         }
 
         public void AddStop(int floor)
         {
-
+            // want to maintain the order intended for the stops.
+            // come back and change logic to use insert to maintain a sane ordering
+            _stops.Add(floor);
         }
     }
 }
--- elevatorsim/elevatorsim/Simulator/ElevatorManager.cs
+++ elevatorsim/elevatorsim/Simulator/ElevatorManager.cs
@@ -8,6 +8,9 @@ namespace elevatorsim.Simulator
 {
     public class ElevatorLandscape : IElevatorLandscape, IElevatorStatusReceiver
     {
+        // This is a code smell - This is possibly going to require duplication of a data structure. 
+        // Reviewing spec again... Elevator can "report" floor, door state via properties on the elevator object
+
         public uint FindNearestElevatorToFloor(int floor)
         {
             throw new NotImplementedException();
