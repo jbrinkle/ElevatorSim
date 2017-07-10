d006b705bd908b06dcd888f8cc77d88dbfcca180
Jason Brinkle <jasongb@microsoft.com>
2017-07-10 08:55:05 -0700

    Interfaces for implementation

---
+++ elevatorsim/elevatorsim/Simulator/IElevatorStatusReceiver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevatorsim.Simulator
+{
+    public interface IElevatorStatusReceiver
+    {
+        void ReportPosition(uint elevatorId, int floor);
+        void ReportDoorOperation(uint evelatorId, bool doorOpened);
+        void ReportMaintenanceNeeded(uint elevatorId);
+    }
+}
